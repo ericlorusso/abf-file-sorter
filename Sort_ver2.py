66df6e8a269631938a4e66a0bec0faf08fb374d9
Eric LoRusso <elorusso@knights.ucf.edu>
2017-11-01 08:48:30 -0400

    initial commit

---
+++ Sort_ver2.py
@@ -0,0 +1,48 @@
+import stfio
+import numpy as np
+import os
+import util
+import shutil
+
+subdirs = {
+	"c":"continuous",
+	"rf":"repetitive_fire",
+	"s":"steps",
+	"ap":"action_potential",
+	"u": "unknown"
+}
+
+target = "./output"
+
+def classify_file(src, filename):
+		
+	if filename.endswith(".abf"):
+		rec = stfio.read(os.path.join(src,filename))
+		array = np.asarray(rec)
+		if rec[0].yunits == 'pA':
+			shutil.copyfile(os.path.join(src, filename),os.path.join(target, subdirs["s"], filename))
+		elif len(array[0][-1]) != len(array[0][-2]):
+			shutil.copyfile(os.path.join(src, filename),os.path.join(target, subdirs["c"], filename))
+		elif len(array[0][0]) >= 50000:
+			shutil.copyfile(os.path.join(src, filename),os.path.join(target, subdirs["rf"], filename))
+		elif len(array[0][0]) <= 50000:
+			shutil.copyfile(os.path.join(src, filename),os.path.join(target, subdirs["ap"], filename))
+		else:
+			shutil.copyfile(os.path.join(src, filename),os.path.join(target, subdirs["u"], filename))	
+
+
+def main():			
+	src = "./input"
+	util.resetDir(target)
+	for key in subdirs.keys():
+		os.mkdir(os.path.join(target, subdirs[key]))
+
+	util.directory_walk(src, classify_file)
+
+			
+
+
+	
+if __name__ == "__main__":
+	main()
+
---
+++ util.py
@@ -0,0 +1,95 @@
+import os
+import sys
+
+
+
+# take a filename (not path) and return it without the extension, if it has one.
+def removeFileExtension(fileName):
+    end = len(fileName) - 1
+    while(fileName[end] != '.' and end != 0):
+        end -= 1
+    if(end != 0):
+        return fileName[:end]
+    if(fileName[end] == '.'):
+        # we're at the begining character and it's a ., meaning it's a file like .gitignore
+        return ''
+    # this fileName doesn't even have an extension
+    return fileName
+
+# take any path, relative or absolute, and return the fileName
+def getFileNameFromPath(path):
+    start = len(path) - 1
+    while((path[start] != '/' and path[start] != '\\') and start != 0):
+        start -= 1
+    if(path[start] == '/' or path[start] == '\\'):
+        start += 1
+    return path[start:]
+
+# take a path and return the filename without the extension
+def getRootFileNameFromPath(path):
+    return removeFileExtension(getFileNameFromPath(path))
+
+
+def ensureEndSlash(path):
+    if(path[-1] == '/' or path[-1] == '\\'):
+        return path
+    return path + '/'
+
+def getParentDir(path):
+    slashes = set(['/','\\'])
+    if(path[-1] in slashes):
+        path = path[:-1]
+    i = len(path) - 1
+    while (path[i] not in slashes) and i != -1:
+        i -= 1
+    #either pointing to end slash, or -1
+    if path[i] in slashes:
+        path = path[:i]
+    return path
+
+
+def resetDir(path):
+    #case 1: path does not exist. make it if you can, or return an error.
+    if(os.path.exists(path) == False):
+        stack = []
+        parentDir = path
+        while(os.path.exists(parentDir) == False):
+            stack.append(parentDir)
+            parentDir = getParentDir(parentDir)
+        while(len(stack) != 0):
+            #you can make this folder now
+            os.mkdir(stack.pop())
+
+    #case 2: the directory exists, and you need to purge it.
+    purgeDir(path)
+
+
+
+#recursively delete an entire folder hierarcy, except for the root folder
+def purgeDir(path):
+    files = os.listdir(path)
+    for File in files:
+        filepath = os.path.join(path,File)
+        if(os.path.isfile(filepath)):
+            if(os.path.splitext(path)[1] != '.gitignore'):
+                os.remove(filepath)
+        else:
+           purgeDir(filepath)
+           os.rmdir(filepath)
+
+
+
+# this is the same as processHierarhcy, but no outputs files are generated.
+def directory_walk(payload_dir, file_function, *args):
+    files = os.listdir(payload_dir)
+    for File in files:
+        file_path = os.path.join(payload_dir, File)
+        if os.path.isfile(file_path):
+            file_function(payload_dir, File, *args)
+        else:
+            directory_walk(file_path, file_function, *args)
+
+
+
+
+
