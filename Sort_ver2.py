c59734469da82c63d6ab4f0b23b6f6c7f9f2398a
Eric LoRusso <elorusso@knights.ucf.edu>
2017-11-01 23:28:47 -0400

    bug fix

--- Sort_ver2.py
+++ Sort_ver2.py
@@ -17,11 +17,12 @@ target = "./output"
 def classify_file(src, filename):
 		
 	if filename.endswith(".abf"):
+		print("currently on file %s." % os.path.join(src, filename))
 		rec = stfio.read(os.path.join(src,filename))
 		array = np.asarray(rec)
 		if rec[0].yunits == 'pA':
 			shutil.copyfile(os.path.join(src, filename),os.path.join(target, subdirs["s"], filename))
-		elif len(array[0][-1]) != len(array[0][-2]):
+		elif len(array[0]) > 1 and len(array[0][-1]) != len(array[0][-2]):
 			shutil.copyfile(os.path.join(src, filename),os.path.join(target, subdirs["c"], filename))
 		elif len(array[0][0]) >= 50000:
 			shutil.copyfile(os.path.join(src, filename),os.path.join(target, subdirs["rf"], filename))
